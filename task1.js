aed035e21aecb9b2d0a8f61a93ca76917266a7e9
Eche Madu <35283575+EcheMadu@users.noreply.github.com>
2020-03-26 14:34:22 +0100

    Add files via upload
    task

---
+++ task1.js
@@ -0,0 +1,16 @@
+var name = EcheMadu
+
+var courses = [HTML, CSS, JS & Figma]
+
+console.log(name);
+
+console.log(courses);
+
+for(i=2; i<=200; i++) {
+
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}
+
+
